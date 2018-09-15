622f9b200481bccbb3220448e205ca933faa8571
AlphaOneSystemDev <AlphaOneSystemDev@DESKTOP-H64RKVC>
2018-09-15 13:17:34 +0800

    Update

--- AOSv1/Form1.cs
+++ AOSv1/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,64 @@ namespace AOSv1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click (object sender, EventArgs e)
+        {
+            this.Hide();
+            register r = new register();
+            r.Show();
+        }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+            //Connecting to the Database Code.
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=aos;sslmode=none;";
+
+            //Encrypt Password Code. Please Refer if anyone is using this program for future reference.
+            StringBuilder sb = new StringBuilder();
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                Byte[] result = hash.ComputeHash(enc.GetBytes(textBox2.Text));
+
+                foreach (Byte b in result)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+            }
+            
+            //Select Query
+            string query = "SELECT * FROM users WHERE username='" + textBox1.Text +"' AND password='" + sb.ToString() + "'";
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    MessageBox.Show("Logged In");
+                }
+                else
+                {
+                    MessageBox.Show("User not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void button3_Click (object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
---
+++ AOSv1/register.cs
@@ -0,0 +1,90 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AOSv1
+{
+    public partial class register : Form
+    {
+        public register()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           if(!textBox1.Text.Equals("") && !textBox2.Text.Equals("") && textBox2.Text.Equals(textBox3.Text))
+            {
+
+
+                //Encrypt Password Code. Please Refer if anyone is using this program for future reference.
+                //Please use github if finish. Still ongoingsss
+                StringBuilder sb = new StringBuilder();
+                using (SHA256 hash = SHA256Managed.Create())
+                {
+                    Encoding enc = Encoding.UTF8;
+                    Byte[] result = hash.ComputeHash(enc.GetBytes(textBox2.Text));
+
+                    foreach (Byte b in result)
+                    {
+                        sb.Append(b.ToString("x2"));
+                    }
+                }
+
+                //Connecting to the Database Code.
+                string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=aos;sslmode=none;";
+
+                //Insert Query
+                string query = "INSERT INTO users(ID, USERNAME, PASSWORD) VALUES(NULL, '" + textBox1.Text + "', '" + sb.ToString() +"')";
+
+                //Find all the User
+                string query2 = "SELECT * FROM users WHERE username='" + textBox1.Text + "' AND password='" + sb.ToString() + "'";
+
+                MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+                MySqlCommand insertCommand = new MySqlCommand(query, databaseConnection);
+                MySqlCommand checkCommand = new MySqlCommand(query2, databaseConnection);
+                insertCommand.CommandTimeout = 60;
+                checkCommand.CommandTimeout = 60;
+                MySqlDataReader reader;
+
+                try
+                {
+                    databaseConnection.Open();
+                    reader = insertCommand.ExecuteReader();
+                    reader.Close();
+                    reader = checkCommand.ExecuteReader();
+
+                    if(reader.HasRows)
+                    {
+                        MessageBox.Show("You have been registered.");
+                        this.Hide();
+                        Form1 home = new Form1();
+                        home.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("SQL code not correct.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void button3_Click (object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
